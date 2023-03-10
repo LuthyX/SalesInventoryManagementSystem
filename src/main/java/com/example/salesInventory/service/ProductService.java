42bb366726356e1e649cad253a7d96358d2c0758
LuthyX <peteradegoke81@gmail.com>
2023-03-10 04:36:28 +0100

    feat: added Add product and change product methods

--- src/main/java/com/example/salesInventory/service/ProductService.java
+++ src/main/java/com/example/salesInventory/service/ProductService.java
@@ -3,6 +3,8 @@ package com.example.salesInventory.service;
 import com.example.salesInventory.exception.ProductDoesNotExistException;
 import com.example.salesInventory.model.Product;
 import com.example.salesInventory.repository.ProductRepository;
+import com.example.salesInventory.request.ChangeProductPriceRequest;
+import com.example.salesInventory.request.CreateProductRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -30,4 +32,22 @@ public class ProductService {
         return product;
     }
 
+    public void addProduct(CreateProductRequest request){
+        Product product = new Product();
+        product.setName(request.getName());
+        product.setDescription(request.getDescription());
+        product.setQuantity(request.getQuantity());
+        product.setPrice(request.getPrice());
+        productRepository.save(product);
+    }
+    public Product changeProductPrice(ChangeProductPriceRequest request) throws ProductDoesNotExistException {
+        Optional<Product> oproduct = productRepository.findById(request.getId());
+        if (!oproduct.isPresent()) {
+            throw new ProductDoesNotExistException();
+        }
+        Product product = oproduct.get();
+        product.setPrice(request.getPrice());
+        productRepository.save(product);
+        return product;
+    }
 }
